9580ab9259854a97b35f793b67cda2c444c4325c
Nwite Victor Ugochukwu <nwitevictor7@gmail.com>
2024-06-06 00:27:25 +0100

    Add files via upload

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import WalletConnection from "./components/WalletConnection";
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -11,7 +12,9 @@ export default function Home() {
     const responseData = await response.json();
 
     if (responseData.pairs && responseData.pairs.length > 0) {
-      setData([responseData]);
+      // Sort pairs by pairCreatedAt in descending order
+      const sortedPairs = responseData.pairs.sort((a, b) => b.pairCreatedAt - a.pairCreatedAt);
+      setData(sortedPairs);
     }
   };
 
@@ -20,19 +23,26 @@ export default function Home() {
   }, []);
 
   return (
-    <section className="flex flex-col w-full h-full justify-center items-center py-20">
-      <div className="bg-white max-w-80 max-h-80 rounded-md p-4">
+    <section className="flex sm:flex-row flex-col w-full h-full gap-2 py-20 px-4">
+      <div className="bg-slate-100 w-full h-full min-h-20 rounded-xl p-4 sm:w-1/2">
         {data.map((item) => (
-          <div key={item.pairAddress}>
-            <div className="flex flex-row justify-between items-center pt-2 pb-4">
-              <h1 className="font-bold">Token Name: {item.baseToken.name}</h1>
-              <Image src={item.info.imageUrl} alt="Pair Image" width={32} height={32} className="mt-4 object-cover hover:cursor-pointer"/>
+          item.baseToken && (
+            <div key={item.pairAddress}>
+              <div className="flex flex-row justify-between items-center pt-2 pb-4">
+                <h1 className="font-bold">Token Name: {item?.baseToken?.name}</h1>
+                <Image src={item?.info?.imageUrl} alt="Pair Image" width={36} height={36} className="mt-4 object-cover hover:cursor-pointer rounded-full"/>
+              </div>
+              <div className="flex flex-col flex-wrap sm:flex-nowrap">
+                <p className="font-medium">Pair Created At: {new Date(item?.pairCreatedAt).toLocaleString()}</p>
+              </div>
             </div>
-            <div className="flex flex-col flex-wrap sm:flex-nowrap">
-              <p className="font-medium">Pair Created At: {new Date(item.pairCreatedAt).toLocaleString()}</p>
-            </div>
-          </div>
+          )
         ))}
+      </div>
+      <div className="flex-col p-4 sm:w-1/2 justify-center bg-slate-100 rounded-xl">
+        {/* Connected wallet and wallet detail section */}
+        <h1 className="font-semibold text-lg text-center">Wallet details</h1>
+        <WalletConnection/>
       </div>
     </section>
   );
